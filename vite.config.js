4ce974472d4652bfbf672ef21693466d27691491
Yeder <ypimentel.tapia@gmail.com>
2025-02-19 19:36:10 +0100

    sigo probando la ruta en vercel

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: "./", // Asegura que se sirvan bien los archivos en Vercel
   build: {
-    outDir: 'dist'
+    outDir: "dist"
   },
   server: {
-    historyApiFallback: true
+    historyApiFallback: true // Maneja rutas en el desarrollo local
   }
 });
